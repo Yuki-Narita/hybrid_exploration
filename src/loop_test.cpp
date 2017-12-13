e8eedb86eb0e472476a5d75fec63d12073722cc9
Yuki-Narita <14ej079@ms.dendai.ac.jp>
2017-12-13 20:30:44 +0900

    loop_test fix

--- src/loop_test.cpp
+++ src/loop_test.cpp
@@ -10,12 +10,14 @@ float pre_loop_x = 0;
 float pre_loop_y = 0;
 int loop_count = 0;
 const int loop_closing_max = 15;
-
+float sum_trans = 0;
+/*
 void tf_callback(const geometry_msgs::Point::ConstPtr& tf_data){
 	float trans_x = tf_data -> x;
 	float trans_y = tf_data -> y;
+	float trans_threshold = 1.0;
 
-	//std::cout << "x:" << trans_x << "," << "y:" << trans_y << std::endl;
+	std::cout << "x:" << trans_x << "," << "y:" << trans_y << std::endl;
 	
 	if(trans_x != pre_loop_x || trans_y != pre_loop_y){
 		pre_loop_x = trans_x;
@@ -23,6 +25,27 @@ void tf_callback(const geometry_msgs::Point::ConstPtr& tf_data){
 		loop_count++;
 		std::cout << "x:" << trans_x << "," << "y:" << trans_y << std::endl;
 		std::cout << "ループクロージング" << loop_count << "回目" << std::endl;
+	}
+}
+*/
+
+
+void tf_callback(const geometry_msgs::Point::ConstPtr& tf_data){
+	float trans_x = tf_data -> x;
+	float trans_y = tf_data -> y;
+	float trans_threshold = 1.0;
+
+	std::cout << "x:" << trans_x << "," << "y:" << trans_y << std::endl;
+	
+	if(trans_x != pre_loop_x || trans_y != pre_loop_y){
+		sum_trans += sqrt(pow(trans_x-pre_loop_x,2)+pow(trans_y-pre_loop_y,2));
+		if(sum_trans > trans_threshold){
+			loop_count++;
+			std::cout << "ループクロージング" << loop_count << "回目" << std::endl;
+			sum_trans = 0;
+		}
+		pre_loop_x = trans_x;
+		pre_loop_y = trans_y;
 	}
 }
 
@@ -37,10 +60,10 @@ int main(int argc, char** argv){
 	while(ros::ok()){
 		tf_queue.callOne(ros::WallDuration(1));
 		//std::cout << "loop_count : " << loop_count << std::endl;
-		if(loop_count == loop_closing_max){
+		/*if(loop_count == loop_closing_max){
 			std::cout << "ループクロージングを" << loop_closing_max << "回したので終了" << std::endl;
 			break;
-		}
+		}*/
 	}
 
 	return 0;
